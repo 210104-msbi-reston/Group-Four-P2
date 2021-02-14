51570edaa087e0a3b6167c28aefd79d46ade91ad
spencer <spencerbarck@gmail.com>
2021-02-14 17:36:07 -0600

    fixed db to dw and added new cube, db, and dw

--- TravelAgencySQL.sql
+++ TravelAgencySQL.sql
@@ -12,17 +12,16 @@ use TravelAgency
 --create City_Countries table
 create table City_Countries
 (
-	pk_city_country_id int PRIMARY KEY,
+	pk_city_country_id int PRIMARY KEY identity,
 	country_name varchar(50) not null,
-	country_code char(2) not null,
-	country_tax float
+	country_code varchar(2) not null
 )
 --end create City_Countries table
 
 --create Customer_Addresses table
 create table Customer_Cities
 (
-	pk_customer_city_id int PRIMARY KEY,
+	pk_customer_city_id int PRIMARY KEY identity,
 	city_name varchar(50) not null,
 	city_state varchar(50),
 	fk_city_country_id int not null,
@@ -57,7 +56,7 @@ create table Month_Quarters
 create table Booking_Dates
 (
 	pk_booking_date_id int PRIMARY KEY,
-	booking_date_string varchar(50) not null,
+	booking_date_string date not null,
 	booking_day int not null,
 	booking_week int not null,
 	fk_booking_month int not null,
@@ -70,8 +69,8 @@ create table Booking_Dates
 create table Hotels
 (
 	pk_hotel_id int PRIMARY KEY,
-	hotel_name varchar(50) not null,
-	hotel_address varchar(50) not null,
+	hotel_name varchar(255) not null,
+	hotel_address varchar(255) not null,
 	hotel_num_rooms int
 )
 --end create Hotels table
@@ -88,7 +87,6 @@ create table Room_Types
 --create Hotels table
 create table Cost_Per_Night
 (
-	pk_cost_id int Primary key,
 	fk_hotel_id int,
 	fk_room_type_id int,
 	fk_booking_month int,
@@ -126,7 +124,6 @@ create table Bookings
 	fk_booking_method_id int,
 	fk_booking_payment_type_id int,
 	fk_booking_room_type_id int,
-	booking_cost float,
 	FOREIGN KEY (fk_booking_hotel_id) REFERENCES Hotels(pk_hotel_id),
 	FOREIGN KEY (fk_booking_start_date_id) REFERENCES Booking_Dates(pk_booking_date_id),
 	FOREIGN KEY (fk_booking_end_date_id) REFERENCES Booking_Dates(pk_booking_date_id),
@@ -137,11 +134,70 @@ create table Bookings
 )
 --end create Bookings table
 
-select * from Room_Types
+--truncate tables
+delete from Cost_Per_Night
+delete from Bookings
 
+delete from Room_Types
+delete from Booking_Methods
+delete from Payment_Types
+
+delete from Customers
+delete from Customer_Cities
+delete from City_Countries
+
+delete from Booking_Dates
+delete from Month_Quarters
+
+delete from Hotels
+
+--view all tables
+select * from Room_Types
+select * from Booking_Methods
+select * from Payment_Types
+
+select * from Customers
+select * from Customer_Cities
+select * from City_Countries
+
+select * from Hotels
+
+select * from Booking_Dates
+select * from Month_Quarters
+
+select count(*) from Bookings
+
+select count(*) from Cost_Per_Night
+
+select * from Cost_Per_Night
+
+
+--insert dummy data
 insert into Room_Types values (1,'Two Queen')
 insert into Room_Types values (2,'King')
 insert into Room_Types values (3,'Suite')
+
+insert into Payment_Types values (1,'Credit')
+insert into Payment_Types values (2,'Debit')
+insert into Payment_Types values (3,'Bank Transfer')
+insert into Payment_Types values (4,'Rewards Points')
+
+insert into Booking_Methods values (1,'Online')
+insert into Booking_Methods values (2,'Over Phone')
+insert into Booking_Methods values (3,'On Site')
+
+insert into City_Countries values (1,'country','CC')
+insert into Customer_Cities values (1,'city','state',1)
+insert into Customers values (1,'name','address',1,'gender',1,'account level',1)
+
+insert into Month_Quarters values (1,1)
+insert into Booking_Dates values (1,'1/1/1',1,1,1,1)
+
+insert into Hotels values (1,'name','address',1)
+
+insert into Cost_Per_Night values (1,1,1,1,1)
+
+insert into Bookings values(1,1,1,1,1,1,1,1)
 
 
 if Object_id('Cost_Per_Night') is not null
@@ -187,3 +243,84 @@ go
 if Object_id('Month_Quarters') is not null
 	drop table Month_Quarters
 go
+
+
+--remove procedure
+if Object_id('proc_Generate_Cost_Per_Night') is not null
+	drop procedure proc_Generate_Cost_Per_Night
+go
+
+
+--procedure to populate cost per night table
+create procedure proc_Generate_Cost_Per_Night
+as
+begin
+	
+	declare @hotel int = 0
+	declare @roomtype int = 0
+	declare @month int = 0
+	declare @baseCost float
+	declare @roomRateCost float
+	declare @finalCost float	
+
+	--loop through each hotel (169)
+	while(@hotel < (select count(pk_hotel_id) from Hotels))
+	begin
+	
+		set @hotel = @hotel + 1
+
+		--Set base cost per hotel with Random number 80-200
+		set @baseCost = (select rand()*(200-50)+50)
+		
+		--loop through each room type (3)
+		while(@roomtype < (select count(pk_room_type_id) from Room_Types))
+		begin
+
+			set @roomtype = @roomtype + 1
+
+			--Multiply cost by room type rate
+
+			set @roomRateCost = @baseCost + (@baseCost * (case @roomtype
+				when 1 then 0.05
+				when 2 then 0.015
+				when 3 then 0.50
+			end))
+
+			--loop through each month (12)
+			while(@month < (select count(pk_month_id) from Month_Quarters))
+			begin
+			
+				set @month = @month + 1
+
+				--Multiply cost by month rate
+				set @finalCost = @roomRateCost + (@roomRateCost * (case @month
+					when 1 then 0.07
+					when 2 then 0.12
+					when 3 then 0.12
+					when 4 then 0.17
+					when 5 then 0.22
+					when 6 then 0.27
+					when 7 then 0.32
+					when 8 then 0.27
+					when 9 then 0.17
+					when 10 then 0.12
+					when 11 then 0.07
+					when 12 then 0.22
+				end))
+
+				insert into Cost_Per_Night values (@hotel,@roomtype,@month,@finalCost)
+				set @finalCost = @roomRateCost
+
+			end
+			set @month = 0
+			set @roomRateCost = @baseCost
+
+		end
+		set @roomtype = 0
+
+	end
+	
+end
+go
+
+exec proc_Generate_Cost_Per_Night
